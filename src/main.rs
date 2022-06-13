2688cbe37961a6b1cc535f3839bbcb1032f691b3
Alaric <alaric@arcanazero.com>
2022-06-12 19:44:44 -0600

    Now reading latest release from github and displaying the publish time.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,37 @@
 use eframe::egui;
 use log::{info, trace, warn};
+use octocrab::{self, models::repos::Release};
 use pretty_env_logger;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     pretty_env_logger::init();
     let options = eframe::NativeOptions::default();
-    eframe::run_native(
-        "Arcana Launcher",
-        options,
-        Box::new(|_cc| Box::new(LauncherApp::default())),
-    );
+
+    let mut app = LauncherApp::new().await?;
+
+    eframe::run_native("Arcana Launcher", options, Box::new(|_cc| Box::new(app)));
 }
 
-struct LauncherApp;
+struct LauncherApp {
+    latest_twelve_knights: Release,
+}
 
-impl Default for LauncherApp {
-    fn default() -> Self {
-        Self
+impl LauncherApp {
+    async fn new() -> Result<Self, Box<dyn std::error::Error>> {
+        let release = Self::get_latest_twelve_knights().await?;
+        Ok(Self {
+            latest_twelve_knights: release,
+        })
+    }
+
+    async fn get_latest_twelve_knights() -> Result<Release, Box<dyn std::error::Error>> {
+        let release = octocrab::instance()
+            .repos("AlaricTheFool", "Twelve-Knights-Vigil")
+            .releases()
+            .get_by_tag("latest")
+            .await?;
+        Ok(release)
     }
 }
 
@@ -24,9 +39,12 @@ impl eframe::App for LauncherApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("Welcome!");
-            if ui.button("Play").clicked() {
-                info!("You haven't got that working yet.");
-            }
+
+            ui.label(format!(
+                "Last Update: {}",
+                self.latest_twelve_knights.published_at.unwrap()
+            ));
+            if ui.button("Play").clicked() {}
         });
     }
 }
